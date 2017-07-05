938b4bdfccafd7e1d4a831ea3cc119d6e063c15a
sgaestel <sgaestel@sierrawireless.com>
2017-07-05 16:03:40 +0200

    PLTFRS-8006 - Use prettier as formatter and eslint as linter

--- techRuuid.js
+++ techRuuid.js
@@ -1,8 +1,8 @@
-var _ = require("lodash");
-var assert = require("assert");
-var uuid = require("node-uuid");
+var _ = require('lodash');
+var assert = require('assert');
+var uuid = require('node-uuid');
 
-var PREFIX = "avop-ruuid-";
+var PREFIX = 'avop-ruuid-';
 
 function isRuuid(ruuid) {
     return _.startsWith(ruuid, PREFIX);
@@ -16,9 +16,9 @@ module.exports = {
     check: function(ruuid) {
         var res = isRuuid(ruuid);
         if (!res) {
-            console.error("Expecting ruuid, found " + JSON.stringify(ruuid));
+            console.error('Expecting ruuid, found ' + JSON.stringify(ruuid));
             console.trace();
         }
-        assert.ok(res, "Expecting ruuid, found " + JSON.stringify(ruuid));
+        assert.ok(res, 'Expecting ruuid, found ' + JSON.stringify(ruuid));
     }
 };
