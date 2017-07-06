851a34913892178ec07853580fd19edb8c94c9b1
sgaestel <sgaestel@sierrawireless.com>
2017-07-06 12:02:00 +0200

    PLTFRS-8006 - Format with prettier using 2 spaces tabs

--- techRuuid.js
+++ techRuuid.js
@@ -5,20 +5,20 @@ var uuid = require('node-uuid');
 var PREFIX = 'avop-ruuid-';
 
 function isRuuid(ruuid) {
-    return _.startsWith(ruuid, PREFIX);
+  return _.startsWith(ruuid, PREFIX);
 }
 
 module.exports = {
-    create: function() {
-        return PREFIX + uuid.v4();
-    },
-    isRuuid: isRuuid,
-    check: function(ruuid) {
-        var res = isRuuid(ruuid);
-        if (!res) {
-            console.error('Expecting ruuid, found ' + JSON.stringify(ruuid));
-            console.trace();
-        }
-        assert.ok(res, 'Expecting ruuid, found ' + JSON.stringify(ruuid));
+  create: function() {
+    return PREFIX + uuid.v4();
+  },
+  isRuuid: isRuuid,
+  check: function(ruuid) {
+    var res = isRuuid(ruuid);
+    if (!res) {
+      console.error('Expecting ruuid, found ' + JSON.stringify(ruuid));
+      console.trace();
     }
+    assert.ok(res, 'Expecting ruuid, found ' + JSON.stringify(ruuid));
+  }
 };
